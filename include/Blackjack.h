4e6daef51dcd530db8839ece5d011bfab7ee259c
oblittle13 <oblittle13@gmail.com>
2023-02-08 20:27:04 -0700

    Style changes

--- include/Blackjack.h
+++ include/Blackjack.h
@@ -13,7 +13,7 @@ class Deck;
 class UI;
 
 class Blackjack {
-    public:
+ public:
     /**
     *   Create a Blackjack object
     *   @param players : Vector of Player pointers to Players who are playing the game
@@ -27,52 +27,52 @@ class Blackjack {
     /**
     *   Play a round of blackjack
     */
-   virtual void playRound();
+    virtual void playRound();
 
-   /**
-   *    Reset the current round of blackjack to its default state
-   */
-   virtual void resetRound(); 
+    /**
+    *    Reset the current round of blackjack to its default state
+    */
+    virtual void resetRound();
 
-   /**
-   *    Deal a card to a specified player
-   *    @param player : The Player designated to recieve the dealt card
-   */
-   virtual void dealCard(Player* player);
+    /**
+    *    Deal a card to a specified player
+    *    @param player : The Player designated to recieve the dealt card
+    */
+    virtual void dealCard(Player* player);
 
-   /**
-   *    Check to see if a players hand is a bust
-   *    @return True is the hand is above 21, false otherwise
-   */
-   virtual bool checkBust();
+    /**
+    *    Check to see if a players hand is a bust
+    *    @return True is the hand is above 21, false otherwise
+    */
+    virtual bool checkBust();
 
-   /**
-   *    Check to see if the game has been won
-   *    @return True if the game has been won, false otherwise
-   */
-   virtual bool checkWin();
+    /**
+    *    Check to see if the game has been won
+    *    @return True if the game has been won, false otherwise
+    */
+    virtual bool checkWin();
 
-   /**
-   *    Dealer plays their turn 
-   */
-   virtual void dealerPlay();
+    /**
+    *    Dealer plays their turn 
+    */
+    virtual void dealerPlay();
 
-   /**
-   *    Player plays their turn
-   */
-   virtual void playerPlay();
+    /**
+    *    Player plays their turn
+    */
+    virtual void playerPlay();
 
-   /**
-   *    Change the view for blackjack
-   *    @param ui : UI pointer that indicates the User interface to change to
-   */
-   virtual void setUI(UI* ui);
+    /**
+    *    Change the view for blackjack
+    *    @param ui : UI pointer that indicates the User interface to change to
+    */
+    virtual void setUI(UI* ui);
 
-   private:
-   Player* dealer;  /**< Dealer of the game */
-   vector::vector<Player*> players; /**< List of players in the game */
-   Deck* deck;  /**< Deck of cards to be used */
-   UI* ui;  /**< Current user interface */
+ private:
+    Player* dealer;  /**< Dealer of the game */
+    vector::vector<Player*> players; /**< List of players in the game */
+    Deck* deck;  /**< Deck of cards to be used */
+    UI* ui;  /**< Current user interface */
 };
 
 #endif
