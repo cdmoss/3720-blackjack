e7c3e9edda145dfe753f6a00d447677da53da8f3
me <chase.mossing@uleth.ca>
2023-02-10 12:56:45 -0700

    fixed formatting and vector namespace errors

--- include/Card.h
+++ include/Card.h
@@ -1,71 +1,66 @@
 #ifndef CARD_H
 #define CARD_H
 
-//#include Hand.h;
+// #include Hand.h;
 
 // Forward declarations to be removed once other classes have been created
 class Hand;
 
 enum Rank {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+  ACE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  JACK,
+  QUEEN,
+  KING
 };
 
-enum Suit {
-    CLUB,
-    SPACE,
-    HEART,
-    DIAMOND
-};
+enum Suit { CLUB, SPACE, HEART, DIAMOND };
 
 class Card {
  public:
-    /**
-    *   Create a Card object
-    *   @param suit : The suit (symbol) the card has
-    *   @param rank : The rank (number or letter) the card has
-    */
-    Card(Suit suit, Rank rank);
+  /**
+   *   Create a Card object
+   *   @param suit : The suit (symbol) the card has
+   *   @param rank : The rank (number or letter) the card has
+   */
+  Card(Suit suit, Rank rank);
 
-    /**
-    *   Retrieve the rank of the card
-    *   @return The rank of the card
-    */
-    virtual Rank getRank();
+  /**
+   *   Retrieve the rank of the card
+   *   @return The rank of the card
+   */
+  virtual Rank getRank();
 
-    /**
-    *   Return the value of the deck. A card's value is the rank they 
-    *   have. If the rank is a J/Q/K, their value is 10. If the Rank
-    *   is an Ace, run aceValue to determine its value.
-    *   @return The value of the card
-    */
-    virtual int getValue();
+  /**
+   *   Return the value of the deck. A card's value is the rank they
+   *   have. If the rank is a J/Q/K, their value is 10. If the Rank
+   *   is an Ace, run aceValue to determine its value.
+   *   @return The value of the card
+   */
+  virtual int getValue();
 
-    /**
-    *   Calculates the value of an Ace card depending on the hand
-    *   score. If the player's hand score is less than 21, the ace's
-    *   value will be 11. If the player's score exceeds 21 with
-    *   an Ace in their hand, the Ace's value will be considered as 1.
-    *   @param hand : The hand determining the ace card's value
-    *   @return The value of the ace card
-    */
-    virtual int aceValue(Hand* hand);
+  /**
+   *   Calculates the value of an Ace card depending on the hand
+   *   score. If the player's hand score is less than 21, the ace's
+   *   value will be 11. If the player's score exceeds 21 with
+   *   an Ace in their hand, the Ace's value will be considered as 1.
+   *   @param hand : The hand determining the ace card's value
+   *   @return The value of the ace card
+   */
+  virtual int aceValue(Hand* hand);
 
  private:
-    Suit suit;
-    Rank rank;
-    int value;
-    bool isAce;
+  Suit suit;
+  Rank rank;
+  int value;
+  bool isAce;
 };
 #endif
--- include/Deck.h
+++ include/Deck.h
@@ -1,7 +1,7 @@
 #ifndef DECK_H
 #define DECK_H
 
-//#include "Card.h"
+// #include "Card.h"
 
 #include <vector>
 
@@ -10,37 +10,37 @@ class Card;
 
 class Deck {
  public:
-    /**
-    *   Create a Deck object
-    *   @param cards : Vector of Card pointers to Cards that are in the deck
-    */
-    Deck(vector::vector<Card*> cards);
+  /**
+   *   Create a Deck object
+   *   @param cards : Vector of Card pointers to Cards that are in the deck
+   */
+  Deck(std::vector<Card*> cards);
 
-    /**
-    *   Retrieve the deck of cards.
-    *   @return The vector of cards.
-    */
-    virtual vector<Card> getCards();
+  /**
+   *   Retrieve the deck of cards.
+   *   @return The vector of cards.
+   */
+  virtual std::vector<Card> getCards();
 
-    /**
-    *    Randomize the order of the deck
-    */
-    virtual void shuffle();
+  /**
+   *    Randomize the order of the deck
+   */
+  virtual void shuffle();
 
-    /**
-    *    Return a random card from the deck to deal
-    *    @return The randomly picked card object
-    */
-    virtual Card* dealCard();
+  /**
+   *    Return a random card from the deck to deal
+   *    @return The randomly picked card object
+   */
+  virtual Card* dealCard();
 
-    /**
-    *    Add a Card to the deck
-    *    @param card : The card being added
-    */
-    virtual void addCard(Card card);
+  /**
+   *    Add a Card to the deck
+   *    @param card : The card being added
+   */
+  virtual void addCard(Card card);
 
  private:
-    vector::vector<Card*> Cards; /**< List of cards in the deck */
+  std::vector<Card*> Cards; /**< List of cards in the deck */
 };
 
 #endif
