9740f55ce15e222081d24259729d16f27f20daea
oblittle13 <oblittle13@gmail.com>
2023-02-19 10:54:23 -0700

    Stubs created

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,3 +1,95 @@
 #include "Player.h"
 
-Player::Player(std::string name) {}
+/*
+* Function: getName()
+* Parameters: None
+* Return Type: String
+* Logic: Retuns name of player
+*/
+std::string Player::getName() const {
+  std::string returnName;
+  return returnName;
+}
+
+/*
+* Function: hit(Card* card)
+* Parameters: Card* card
+* Return Type: void
+* Logic: Add a card to the players current hand
+*/
+void Player::hit(Card *card) {
+}
+
+/*
+* Function: split()
+* Parameters: None
+* Return Type: void
+* Logic: Splits the players current hand in 2
+*/
+void Player::split() {
+}
+
+/*
+* Function: stand()
+* Parameters: None
+* Return Type: void
+* Logic: Indicate the player would not like a card hadden to
+* their current hand
+*/
+void Player::stand() {
+}
+
+/*
+* Function: checkBust()
+* Parameters: None
+* Return Type: bool
+* Logic: Determine if players hand is bust, if so return true
+* else false
+*/
+bool Player::checkBust() {
+  bool returnBust;
+  return returnBust;
+}
+
+/*
+* Function: getRoundsWon()
+* Parameters: None
+* Return Type: int
+* Logic: Retuns number of rounds the player has won
+*/
+int Player::getRoundsWon() {
+  int returnRounds;
+  return returnRounds;
+}
+
+/*
+* Function: winRound()
+* Parameters: None
+* Return Type: void
+* Logic: Makes the current player win the round
+*/
+void Player::winRound() {
+}
+
+/*
+* Function: getTurnIsActive()
+* Parameters: None
+* Return Type: bool
+* Logic: Retuns true if the players turn is active, else false
+*/
+bool Player::getTurnIsActive() {
+  bool returnActive;
+  return returnActive;
+}
+
+/*
+* Function: getHand()
+* Parameters: None
+* Return Type: std::vector<Hand*>
+* Logic: Returns the vector that holds pointers to all the
+* players hands
+*/
+std::vector<Hand*> Player::getHand() {
+  std::vector<Hand*> returnHand;
+  return returnHand;
+}
---
+++ src/PlayerUI.cpp
@@ -0,0 +1,11 @@
+
+#include "../include/PlayerUI.h"
+
+/*
+* Function: displayHand(Player* player)
+* Parameters: Player* player
+* Return Type: void
+* Logic: Displays the hand(s) of the current player
+*/
+void PlayerUI::displayHand(Player* player) {
+}
